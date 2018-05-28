971f3b5e4b4c02a8f323e6c33626205ecbe97268
cjyoon <cjyoon@kaist.ac.kr>
2018-05-28 23:22:40 +0900

    svAnnotate.py added

---
+++ svAnnotate.py
@@ -0,0 +1,151 @@
+import gzip 
+import re
+import pysam 
+import os, sys
+import random
+import subprocess, shlex
+# to allow vep to annotate ALT column cannot be in SV annotation format in vcf. Change it to SNV annotation so that I can still utilize VEP's annotation
+
+
+class Position():
+    ''' python class for handling genomic positions
+    0-based
+    '''
+    def __init__(self, chromosome, start, end, is_bp=None, clipped_reads=None):
+        self.chromosome = chromosome
+        self.start = start
+        self.end = end
+        self.is_bp = is_bp
+        self.clipped_reads = None
+
+    def __repr__(self):
+        return str(self.chromosome + ":" + str(self.start) + '-' + str(self.end))
+
+    def __str__(self):
+        return str(self.chromosome + ":" + str(self.start) + '-' + str(self.end))
+
+    def __len__(self):
+        return int(self.end - self.start)
+
+    def __iter__(self):
+        for i in range(self.start, self.end):
+            yield Position(self.chromosome, i, i+1)
+
+    def __next__(self):
+        self.start += 1
+        self.end += 1
+        return self
+
+    def __hash__(self):
+        return hash((self.chromosome, self.start, self.end))
+
+    def __eq__(self, other):
+        if isinstance(other, Position):
+            return self.chromosome == other.chromosome and self.start == other.start and self.end == other.end
+        else:
+            print("Not of the same class, cannot compare equality")
+            return None
+
+    @classmethod
+    def fromstring(cls, position_string):
+        chromosome = position_string.split(':')[0]
+        start = int(position_string.split(':')[1].split('-')[0])
+        end = int(position_string.split(':')[1].split('-')[1])
+        return Position(chromosome, start, end)
+
+
+def get_canonical_annotation(long_annotation_string):
+    '''vep output gives multiple annotations for a given loci, one is often only interested in the canonical sites
+    thus looks for |YES| string to find canonical annotation
+    if canonical annotation is not found, just return the very first annotation'''
+    return_status = 0
+    for anno in long_annotation_string.split(','):
+        if re.search(r'|YES|', anno):
+            return_status =1 
+            return anno
+
+    if return_status == 0:
+        return annotation.split(',')[0]
+
+
+
+
+def get_vep_annotation(genomic_position, reference_fasta):
+    '''temporarily create a minimal SNV vcf for a given genomic position
+    and then use this vcf to get variant information by running VEP
+    then parse the VEP output vcf to get the necessary variant annotation info
+    '''
+    
+    temp_vcf = write_temp_vcf(genomic_position, reference_fasta)
+    # now run VEP annotation command
+    cmd = f'python /home/users/cjyoon/scripts/annotate_vcf/annotate_vcf.py -i {temp_vcf}'
+    subprocess.call(shlex.split(cmd))
+    
+    vep_vcf = re.sub(r'.vcf$', '.vep.vcf.gz', temp_vcf)
+    with gzip.open(vep_vcf, 'rt') as f:
+        for line in f:
+            if not line.startswith('#'):
+                annotation = line.strip().split()[7].split('=')[1]
+                # multiple annotations per variant, now need to find which one is the canonical
+                canonical_annotation = get_canonical_annotation(annotation)
+                print(canonical_annotation) 
+
+    return canonical_annotation
+
+def make_temp_variant(genomic_position, reference_fasta):
+    '''make a temporary VCF line that will be used as an input for VEP command'''
+    gPosition = Position.fromstring(genomic_position)
+    reference = pysam.FastaFile(reference_fasta)
+    refbase = reference.fetch(gPosition.chromosome, gPosition.start, gPosition.start + 1)
+    bases = ['A', 'T', 'G', 'C']
+
+    if refbase != 'N':
+        bases.remove(refbase)
+    else:
+        pass
+
+    altbase = random.choice(bases)
+    variantString = f'{gPosition.chromosome}\t{gPosition.start}\t.\t{refbase}\t{altbase}\t.\t.\t.'
+    return variantString
+
+def write_temp_vcf(genomic_position, reference_fasta, temp_dir='.'):
+    '''write a temporary vcf file that will be used as an input for VEP command'''
+    temp_vcf = os.path.join(temp_dir, str(genomic_position) + '.vcf')
+    with open(temp_vcf, 'w') as f:
+        f.write('##fileformat=VCFv4.1\n##reference=file:///path/to/human_g1k_v37.fasta\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n')
+        variantString = make_temp_variant(genomic_position, reference_fasta)
+        f.write(variantString)
+
+    return temp_vcf
+
+
+def sv_annotation(bp1_position, bp2_position, reference_fasta):
+    bp1_canonical = get_vep_annotation(bp1_position, reference_fasta)
+    bp2_canonical = get_vep_annotation(bp2_position, reference_fasta)
+
+    print(bp1_canonical.split('|')[3], bp1_canonical.split('|')[35], bp1_canonical.split('|')[8], bp1_canonical.split('|')[9])
+    print(bp2_canonical.split('|')[3], bp2_canonical.split('|')[35], bp2_canonical.split('|')[8], bp2_canonical.split('|')[9])
+
+sv_annotation(sys.argv[1], sys.argv[2], '/home/users/data/01_reference/human_g1k_v37/human_g1k_v37.fasta')
+
+
+
+'''
+
+input_vcf = sys.argv[1]
+output_vcf = 'test.out'
+with open(output_vcf, 'w') as g:
+    with open(input_vcf, 'r') as f:
+        for line in f:
+            if not line.startswith('#'):
+                bases = ['A', 'T', 'G', 'C']
+                CHROM, POS, ID, REF, ALT, *args = line.split('\t')
+                bases.remove(REF[0])
+                
+                changed_base = random.choice(bases)
+                g.write(CHROM + '\t' + POS + '\t' + ID + '\t' + REF + '\t' + changed_base + '\t' + '\t'.join(args) + '\n')
+            else:
+                # if header, then just write without modification
+                g.write(line)
+
+'''
