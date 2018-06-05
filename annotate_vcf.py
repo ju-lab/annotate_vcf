aa56ec2fe49dd396ea64ff5b77a8d204d415df1d
cjyoon <cjyoon@kaist.ac.kr>
2018-06-05 13:35:49 +0900

    now anyone in the lab can use with cjyoon's PERL and VEP cache

--- annotate_vcf.py
+++ annotate_vcf.py
@@ -24,8 +24,13 @@ def outputfile(vcffile, output_dir):
 
 def vep_annotate(input_vcf, annotated_vcf, display=False):
     ''' if you want single annotation then use --per_gene option'''
+    # DEFINE PERL5LIB PATH for other people's use
+    os.environ['PERL5LIB'] = '/home/users/cjyoon/anaconda3/envs/vep/lib/perl5/site_perl/5.22.0/x86_64-linux-thread-multi:/home/users/cjyoon/anaconda3/lib/perl5/site_perl/5.22.2/x86_64-linux-thread-multi/' 
+    os.environ['PATH'] += '/home/usrs/cjyoon/anaconda3/bin'
+    # DEFINE VEP CACHE directory to use
+    dir = '/home/users/cjyoon/.vep'
 
-    cmd = f'/home/users/cjyoon/ensembl-vep/vep --sift b --ccds --uniprot --hgvs --symbol --numbers --domains --gene_phenotype --canonical --protein --biotype --uniprot --tsl --pubmed --variant_class --shift_hgvs 1 --check_existing --total_length --allele_number --no_escape --xref_refseq --failed 1 --vcf --flag_pick_allele --pick_order canonical,tsl,biotype,rank,ccds,length  --offline --no_progress --no_stats  --polyphen b  --regulatory --af --af_1kg --af_gnomad --af_esp --max_af -i {input_vcf} -o {annotated_vcf} --force_overwrite --nearest symbol'
+    cmd = f'/home/users/cjyoon/ensembl-vep/vep --dir {dir} --sift b --ccds --uniprot --hgvs --symbol --numbers --domains --gene_phenotype --canonical --protein --biotype --uniprot --tsl --pubmed --variant_class --shift_hgvs 1 --check_existing --total_length --allele_number --no_escape --xref_refseq --failed 1 --vcf --flag_pick_allele --pick_order canonical,tsl,biotype,rank,ccds,length  --offline --no_progress --no_stats  --polyphen b  --regulatory --af --af_1kg --af_gnomad --af_esp --max_af -i {input_vcf} -o {annotated_vcf} --force_overwrite --nearest symbol'
 
     if display==True:
         print(cmd)
